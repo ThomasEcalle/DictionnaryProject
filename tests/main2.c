423b6f7d6758a6fbe1bb2496ff43df3cff4cb27b
Jean <spyroo913@hotmail.com>
2016-10-25 02:22:04 +0200

    ajout de l'infrastructure
    ajout des dossiers et des dossiers virtuels (mvc) + des fichiers de test

--- tests/main2.c
+++ tests/main2.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "constantes.h"
-#include "controllerMainMenu.h"
-#include "controllerActiveDictionary.h"
 
 
 int main()
 {
-
+    printf("Hello world!\n");
+    return 0;
 }
