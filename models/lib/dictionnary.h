ed8282fe34ba6ca20a2b7f6fd13b7bb6be660498
My-PC <thomasecalle@hotmail.fr>
2016-12-02 04:15:16 +0100

    Gestion dicos
    lecture fichier texte, enregistrmeent en mémoire de tous les mots et
    écriture dans un .dico dans l'ordre et en enlevant les doublons

---
+++ models/lib/dictionnary.h
@@ -0,0 +1,30 @@
+#ifndef DICTIONNARY_H_INCLUDED
+#define DICTIONNARY_H_INCLUDED
+
+typedef struct Element Element;
+struct Element
+{
+    char * chaine;
+    Element *next;
+    int length;
+};
+typedef struct List List;
+struct List
+{
+    int length;
+    Element *first;
+};
+
+int removeElement(List * list, char * value);
+Element * recursiveSort (Element * a, Element * b);
+void afficherListe(List * list);
+void getWordsFromFile(char * path, List * elementsList);
+List *initialisationList();
+void insertion(List *list, char * string);
+void reinitialisationString(char * string, int size);
+
+#endif // CONSTANTES_H_INCLUDED
+
+
+
+
